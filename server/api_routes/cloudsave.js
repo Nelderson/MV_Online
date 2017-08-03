6b81b1d5296e521ca2ff4591762bfc64cf47d6a1
MChambers1992 <michael.chambers@bedegaming.com>
2017-08-03 13:32:23 +0100

    [MinorVersion] Add correct requires to cloudsave.js file for logging config

--- server/api_routes/cloudsave.js
+++ server/api_routes/cloudsave.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose");
 var express = require('express');
 var authAPI = require('../authenticateAPI.js');
 var router = express.Router();
+var config = require('../configurations/config');
 var log = require('tracer').colorConsole(config.loggingConfig);
 
 var Save = new mongoose.Schema({
