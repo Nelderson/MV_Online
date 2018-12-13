81fd193ee4c881ceacc279ce0886227cf093514d
Nelderson <matthew.cip@gmail.com>
2018-12-13 08:34:06 -0500

    New auth method

--- server/api_routes/example.js
+++ server/api_routes/example.js
@@ -1,12 +1,23 @@
 var express = require('express');
-var authAPI = require('../authenticateAPI.js');
+var authAPI = require('../auth.js').authAPI;
 var router = express.Router();
 
 router.use(function(req,res,next){
-  authAPI(req,res,next); //Need to have a valid token for all routes
+  if (req.session){
+    console.log('HERRRRE', req.session);
+    next();
+  }
+  else {
+    return res.status(403).send('Nope');
+  }
+  // authAPI(req,res,next); //Need to have a valid token for all routes
 });
 
 router.get('/testing',function(req,res){
+  req.session.user = 'Testing sessions out!'
+  req.session.save(function(err) {
+    console.log('SESSSION SAVED', req.session, req.sessionID);
+  })
   res.status(200).send('Dude....this is totally awesome');
 });
 
